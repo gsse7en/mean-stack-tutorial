b3247f3a2b141f320bf2769c78f752345dd21f68
gsse7en <latyshev.fedor@gmail.com>
2018-03-30 22:19:39 +0300

    products layouts

--- todoapp-angular/src/app/app.component.ts
+++ todoapp-angular/src/app/app.component.ts
@@ -3,6 +3,19 @@ import { TodoService } from './services/todo.service';
 import ToDo from './models/todo.model';
 import { Component, OnInit } from '@angular/core';
 
+class Cell {
+  //_id: string;
+  name: string;
+  color: string;
+  isEditing: boolean;
+
+  constructor(name = "", color = "gray"){
+    this.name = name;
+    this.color = color;
+    this.isEditing = false;
+  }
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -10,6 +23,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AppComponent implements OnInit {
 
+  public productNames:string[] = ["1","2","3","4","5"];
+  //TODO: make 4 classes for buttons
+  //ifClicked show 3 colors
+
+
+  public productTable: Cell[] = [];
   constructor(
     //Private todoservice will be injected into the component by Angular Dependency Injector
     private todoService: TodoService
@@ -24,6 +43,11 @@ export class AppComponent implements OnInit {
 
   ngOnInit(): void {
 
+    for (let i=0, length = this.productNames.length; i < length; i++) {
+      for (let j=0; j < length; j++) {
+        this.productTable.push(new Cell(`${i}-${j}`));
+      }
+    }
     //At component initialization the
     this.todoService.getToDos()
       .subscribe(todos => {
@@ -33,6 +57,35 @@ export class AppComponent implements OnInit {
       })
   }
 
+  getCell(name) {
+    return this.productTable.find((cell) => cell.name === name);
+  }
+  editCell(name) {
+    console.log(name);
+    this.productTable.map((cell) => {
+      console.log(cell);
+      if (cell.name === name) {
+        cell.isEditing = true;
+      }
+      return cell;
+    });
+    console.log(this.productTable);
+  }
+  goodMix(name) {
+    const cell = this.getCell(name);
+    cell.color = "green";
+    cell.isEditing = false;
+  }
+  normMix(name) {
+    const cell = this.getCell(name);
+    cell.color = "gray";
+    cell.isEditing = false;
+  }
+  badMix(name) {
+    const cell = this.getCell(name);
+    cell.color = "red";
+    cell.isEditing = false;
+  }
   editTodo(todo: ToDo) {
     console.log(todo)
     if(this.todosList.includes(todo)){
